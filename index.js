a9c35c79ba2283be4bac9fdea0f1926a69d2b963
Alexander <alex.galev@yahoo.com>
2023-02-08 16:13:05 -0700

    solved

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+	// Write your algorithm here
+	let wordArray = word.split('')
+	let reverseWordArray = wordArray.slice().reverse()
+	for (let i = 0; i < wordArray.length; i++) {
+		if (wordArray[i] !== reverseWordArray[i]) {
+			return false
+		}
+		return true
+	}
 }
+isPalindrome('robot')
 
 /* 
   Add your pseudocode here
+  1) split word into array of letters
+  2) create new array of letters in reverse order, preserving the original array
+  3) loop through letters of word and compare to letters of word in reverse order
 */
 
 /*
@@ -12,14 +24,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log('Expecting: true')
+	console.log('=>', isPalindrome('racecar'))
 
-  console.log("");
+	console.log('')
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+	console.log('Expecting: false')
+	console.log('=>', isPalindrome('robot'))
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome
